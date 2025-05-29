f597adc910830075212cf4fe6bf0cb22a0238797
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-05-29 23:51:07 +0700

    feat: add get events endpoint for customers

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import express from 'express';
-import authRoutes from './routes/authRoutes'
-import eventRoutes from './routes/eventRoutes'
+import express from "express";
+import authRoutes from "./routes/authRoutes";
+import eventRoutes from "./routes/eventRoutes";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.use(cors());
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/admin/events', eventRoutes)
+app.use("/auth", authRoutes);
+app.use("/events", eventRoutes);
 
 // Start server
 app.listen(PORT, () => {
--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -1,22 +1,29 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth";
 import { verifyOrganizer } from "../middlewares/verifyOrganizer";
-import { 
+import {
   createEvent,
   deleteEvent,
   getEventById,
-  getEventsByOrganizer, 
-  updateEvent
+  getEventsByOrganizer,
+  updateEvent,
+  getAllEvents,
+  getDetailEventById,
 } from "../controllers/eventController";
 
 const router = Router();
 
+//endpoint sisi customer
+router.get("/all", getAllEvents);
+router.get("/detail/:id", getDetailEventById);
+
+// endpoint sisi event organizer
 router.use(authenticate, verifyOrganizer);
 
-router.post('/', createEvent);
-router.get('/', getEventsByOrganizer);
-router.get('/:id', getEventById);
-router.put('/:id', updateEvent);
-router.delete('/:id', deleteEvent);
+router.post("/", createEvent);
+router.get("/", getEventsByOrganizer);
+router.get("/:id", getEventById);
+router.put("/:id", updateEvent);
+router.delete("/:id", deleteEvent);
 
 export default router;
