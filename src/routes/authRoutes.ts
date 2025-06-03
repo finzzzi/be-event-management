6f863158d9b281eda261cf50ceb029204a489234
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-06-03 12:46:46 +0700

    fix: adjust transaction flow

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { register, login, verify } from "../controllers/authController";
+import {
+  register,
+  login,
+  verify,
+  getUserProfile,
+} from "../controllers/authController";
 import { authenticate } from "../middlewares/auth";
 
 const router = Router();
@@ -7,5 +12,6 @@ const router = Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/verify", authenticate, verify);
+router.get("/user", authenticate, getUserProfile);
 
 export default router;
