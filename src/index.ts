5df457fb024c0a7a1062dfd2c0a4dd337f6476b5
finzzzi <gila.seal@yahoo.co.id>
2025-06-04 09:08:50 +0700

    Fix: fix payment proof

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import authRoutes from "./routes/authRoutes";
 import eventRoutes from "./routes/eventRoutes";
 import transactionRoutes from "./routes/transactionRoutes";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/events", eventRoutes);
 app.use("/transactions", transactionRoutes);
+const uploadsPath = path.join(__dirname, "../uploads");
+app.use("/uploads", express.static(uploadsPath));
 
 // Start server
 app.listen(PORT, () => {
